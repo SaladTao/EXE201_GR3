c3e9e171c7abba1bc6a025ef99de0e0ef9404466
Nguyễn Minh Sáng <sangnmhe176048@fpt.edu.vn>
2025-05-31 23:14:52 +0700

    first commit

---
+++ exe201/Pages/Admin/Orders/Create.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using exe201.Models;
+
+namespace exe201.Pages.Admin.Orders
+{
+    public class CreateModel : PageModel
+    {
+        private readonly exe201.Models.EcommerceContext _context;
+
+        public CreateModel(exe201.Models.EcommerceContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+        ViewData["UserId"] = new SelectList(_context.Users, "Id", "Email");
+            return Page();
+        }
+
+        [BindProperty]
+        public Order Order { get; set; } = default!;
+
+        // For more information, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Orders.Add(Order);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
