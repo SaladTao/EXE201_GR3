31a974f338f767319c286e9bb3ef6e8445e49ed7
GaGionCay <minhtrihoangngoc@gmail.com>
2025-06-08 14:08:38 +0700

    udpate Story

--- exe201/Pages/Customer/OrderDetail.cshtml.cs
+++ exe201/Pages/Customer/OrderDetail.cshtml.cs
@@ -1,19 +1,23 @@
-using exe201.Models;
+﻿using exe201.Models;
 using exe201.Service.Product;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Text.RegularExpressions;
 
 namespace exe201.Pages.Customer
 {
     public class OrderDetailModel : PageModel
     {
         private readonly ProductService _productService;
+
         public OrderDetailModel(ProductService productService)
         {
             _productService = productService;
         }
+
         public Product Product { get; set; }
         public List<OrderDetail> OrderDetails { get; set; }
+
         public IActionResult OnGet(int id)
         {
             Product = _productService.GetProductById(id);
